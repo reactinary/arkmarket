6d994a254cc454efef42ccc7ba8575b114978142
Maxime Montfort <reactinary@gmail.com>
2023-11-10 12:22:05 +0100

    Adding image url for background after deploying on Vercel

--- app/explore/HeroSection.tsx
+++ app/explore/HeroSection.tsx
@@ -9,8 +9,7 @@ export default function HeroSection() {
   return (
     <>
       {/* ------ BANNER ------ */}
-      <Image src="/banner.jpg" alt="background" width={1920} height={500}/>
-
+      <Image src="https://res.cloudinary.com/dzsyv502k/image/upload/v1699614893/Banner_bw77ul.jpg" alt="background" width={1920} height={500}/>
 
       <div className="container flex flex-col md:flex-row justify-between md:items-start sm:items-center mt-6  gap-4">
         <CollectionDescription />
